7e1b4d7b396e0987286ff3427c7a3092170276bf
TheFOOL18 <tulasirushwik@gmail.com>
2026-02-09 00:10:27 +0530

    first commit

---
+++ tokenizer.py
@@ -0,0 +1,9 @@
+import numpy as np
+import os
+
+EN_DATASET_RAW = "raw_dataset/cc100_en.jsonl"
+MN_DATASET_RAW = "raw_dataset/cc100_mn.jsonl"
+
+EN_DATASET = "dataset/cc100_en.jsonl"
+MN_DATASET = "dataset/cc100_mn.jsonl"
+
